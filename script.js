932224af6d7c8c681f14a9bb56bd8ae429a7c3cf
AriannaCF <106922479+AriannaCF@users.noreply.github.com>
2022-08-19 11:38:56 -0400

    Revisions
    Validated and beautified js, removed favicon, added meta tags to html

--- script.js
+++ script.js
@@ -4,34 +4,28 @@ let color = document.querySelector('#colorPicker');
 let height = document.querySelector('#inputHeight');
 let width = document.querySelector('#inputWidth');
 //Select pixel canvas
-let canvas= document.querySelector('#pixelCanvas');
-
+let canvas = document.querySelector('#pixelCanvas');
 // When size is submitted by the user, call makeGrid()
 document.querySelector('#sizePicker').addEventListener('submit', makeGrid);
 
 function makeGrid(evt) {
-    evt.preventDefault();
-    //first loop increases grid height by adding rows to the canvas
-    canvas.innerText="";
-    for (let i=0; i<height.value;i++) {
-        let row = document.createElement('tr');
-        canvas.append(row)
-        //inner loop appends table cells to each row to increase grid width
-        for (let j=0;j<width.value;j++) {
-            let column = document.createElement('td');
-            row.append(column);
-        }
-
-    }
+	evt.preventDefault();
+	//first loop increases grid height by adding rows to the canvas
+	canvas.innerText = "";
+	for(let i = 0; i < height.value; i++) {
+		let row = document.createElement('tr');
+		canvas.append(row);
+		//inner loop appends table cells to each row to increase grid width
+		for(let j = 0; j < width.value; j++) {
+			let column = document.createElement('td');
+			row.append(column);
+		}
+	}
 }
-
 //add color to the pixel grid
 function colorizer(e) {
-//use event delegation to target the individual table cells once they are appended to the table
-    e.target.style.backgroundColor=color.value;
-    }
-
+	//use event delegation to target the individual table cells once they are appended to the table
+	e.target.style.backgroundColor = color.value;
+}
 //final event listener adds chosen  color to canvas
 canvas.addEventListener('mousedown', colorizer);
-
-
