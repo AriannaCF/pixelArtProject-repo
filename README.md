101c932f0aedf282dbe4404a7eab6673c90f0054
AriannaCF <106922479+AriannaCF@users.noreply.github.com>
2022-08-19 00:35:45 -0400

    Final Touches
    Changed script file name, added descriptive comments to the script file, and updated the ReadMe File

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pixelArtProject-repo
- Udacity FEND Pixel Art Project
+
+Udacity FEND Pixel Art Project
+
+This project was developed for the Udacity FrontEnd Web Development Nanodegree program using HTML, CSS, and Javascript.
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let color = document.querySelector('#colorPicker');
 // Select size input
 let height = document.querySelector('#inputHeight');
 let width = document.querySelector('#inputWidth');
+//Select pixel canvas
 let canvas= document.querySelector('#pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
@@ -10,10 +11,12 @@ document.querySelector('#sizePicker').addEventListener('submit', makeGrid);
 
 function makeGrid(evt) {
     evt.preventDefault();
+    //first loop increases grid height by adding rows to the canvas
     canvas.innerText="";
     for (let i=0; i<height.value;i++) {
         let row = document.createElement('tr');
         canvas.append(row)
+        //inner loop appends table cells to each row to increase grid width
         for (let j=0;j<width.value;j++) {
             let column = document.createElement('td');
             row.append(column);
@@ -21,11 +24,14 @@ function makeGrid(evt) {
 
     }
 }
+
 //add color to the pixel grid
 function colorizer(e) {
+//use event delegation to target the individual table cells once they are appended to the table
     e.target.style.backgroundColor=color.value;
     }
 
+//final event listener adds chosen  color to canvas
 canvas.addEventListener('mousedown', colorizer);
 
 
